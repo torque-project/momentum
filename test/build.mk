2516668dd0c1fb1e34a9702600bf7312dfbbfc64
Jan Krüger <jan.krueger@gmail.com>
2016-02-02 13:15:52 +0100

    precompiled header before sources

--- test/build.mk
+++ test/build.mk
@@ -2,10 +2,10 @@ include $(TOP)/build/header.mk
 
 products_$(d) := unit
 
+unit_precompiled_header_$(d) := precompiled.hpp
+
 unit_sources_$(d) += \
     test.cpp
-
-unit_precompiled_header_$(d) := precompiled.hpp
 
 unit_target_dir_$(d) := bin
 unit_cxx_flags_$(d)  := -g -std=c++14 -I$(TOP)/include
