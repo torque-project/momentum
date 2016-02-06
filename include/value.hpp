095a5f236333a231e74824fb997ef739fe43a522
Jan Krüger <jan.krueger@gmail.com>
2016-02-06 12:45:03 +0100

    Add move constructor to value

--- include/value.hpp
+++ include/value.hpp
@@ -16,6 +16,7 @@ namespace imu {
   struct value {
 
     inline value()
+      : pad(nullptr)
     {}
 
     template<typename T>
@@ -26,6 +27,16 @@ namespace imu {
     inline value(const value& cpy)
       : pad(cpy.pad ? cpy.pad->clone() : nullptr)
     {}
+
+    inline value(value&& other)
+      : pad(other.pad)
+    {
+      other.pad = nullptr;
+    }
+
+    inline ~value() {
+      delete pad;
+    }
 
     inline bool is_set() const {
       return (bool) pad;
@@ -45,7 +56,7 @@ namespace imu {
         throw bad_value_cast();
       }
 
-      return dynamic_cast<pad_type*>(pad.get())->value;
+      return dynamic_cast<const pad_type*>(pad)->value;
     }
 
     inline friend bool operator== (const value& l, const value& r) {
@@ -60,9 +71,11 @@ namespace imu {
 
       typedef std::unique_ptr<value_pad_base> p;
 
+      virtual ~value_pad_base() {}
+
       virtual value_pad_base* clone() const = 0;
 
-      virtual bool equiv(const p& other) const = 0;
+      virtual bool equiv(const value_pad_base* other) const = 0;
 
       virtual const std::type_info& type() const = 0;
     };
@@ -76,12 +89,16 @@ namespace imu {
         : value(v)
       {}
 
+      value_pad(const T&& v)
+        : value(static_cast<T&&>(v))
+      {}
+
       value_pad_base* clone() const {
         return new value_pad(value);
       }
 
-      bool equiv(const value_pad_base::p& other) const {
-        return value == dynamic_cast<value_pad*>(other.get())->value;
+      bool equiv(const value_pad_base* other) const {
+        return value == dynamic_cast<const value_pad*>(other)->value;
       }
 
       const std::type_info& type() const {
@@ -96,7 +113,7 @@ namespace imu {
       }
     };
 
-    std::unique_ptr<value_pad_base> pad;
+    const value_pad_base* pad;
   };
 
   template<typename T>
@@ -107,5 +124,10 @@ namespace imu {
   template<>
   inline const value& value_cast<value>(const value& v) {
     return v;
+  }
+
+  template<typename T, typename X>
+  inline const T& value_cast(const X& x) {
+    return static_cast<const T&>(x);
   }
 }
