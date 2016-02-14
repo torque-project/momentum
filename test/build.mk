c851abe8e24ff3ac2b95c9359f49aa21fee94077
Jan Krüger <jan.krueger@gmail.com>
2016-02-14 18:11:35 +0100

    move headers to extra subdir, to make -I compiler options possible

--- test/build.mk
+++ test/build.mk
@@ -8,7 +8,7 @@ unit_sources_$(d) += \
 
 unit_precompiled_header_$(d) := 
 unit_target_dir_$(d) := bin
-unit_cxx_flags_$(d)  := -g -std=c++14 -I$(TOP)/include
+unit_cxx_flags_$(d)  := -g -std=c++14 -I$(TOP)/include/momentum
 unit_ld_flags_$(d)   := 
 
 #perf_sources_$(d) += \
@@ -16,7 +16,7 @@ unit_ld_flags_$(d)   :=
 
 #perf_precompiled_header_$(d) := 
 #perf_target_dir_$(d) := bin
-#perf_cxx_flags_$(d)  := -O3 -std=c++14 -I$(TOP)/include -I/usr/local/include
+#perf_cxx_flags_$(d)  := -O3 -std=c++14 -I$(TOP)/include/momentum -I/usr/local/include
 #perf_ld_flags_$(d)   := 
 
 include $(TOP)/build/footer.mk
