27484c11aa50fd07c7288bcff30cdebd63acbec8
Jan Krüger <jan.krueger@gmail.com>
2016-02-16 15:46:35 +0100

    support value_cast on pointers

--- include/momentum/value.hpp
+++ include/momentum/value.hpp
@@ -161,6 +161,11 @@ namespace imu {
   }
 
   template<typename T, typename X>
+  inline const T* value_cast(const X* x) {
+    return static_cast<const T*>(x);
+  }
+
+  template<typename T, typename X>
   inline T value_cast(const std::shared_ptr<X>& x) {
     return std::static_pointer_cast<typename T::element_type>(x);
   }
