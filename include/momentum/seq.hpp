204080160f2315779a35eaa429968e028eba5792
Benjamin Teuber <benjamin.teuber@freiheit.com>
2016-05-14 20:10:22 +0100

    Allow custom comprators for seq equiv

--- include/momentum/seq.hpp
+++ include/momentum/seq.hpp
@@ -6,17 +6,23 @@ namespace imu {
 
   namespace seqs {
 
-    template<typename S, typename T>
-    inline bool equiv(const S& s, const T& x) {
+    template<typename S, typename T, typename F>
+    inline bool equiv(const S& s, const T& x, const F& eq) {
 
       auto h1 = s;
       auto h2 = seq(x);
 
-      while (!is_empty(h1) && !is_empty(h2) && first(h1) == first(h2)) {
+      while (!is_empty(h1) && !is_empty(h2) && eq(*first(h1), *first(h2))) {
         h1 = rest(h1); h2 = rest(h2);
       }
 
       return is_empty(h1) && is_empty(h2);
+    }
+
+    template<typename S, typename T>
+    inline bool equiv(const S& s, const T& x) {
+      typedef typename semantics::real_type<S>::type::value_type value_type;
+      return equiv(s, x, std::equal_to<value_type>());
     }
   }
 }
